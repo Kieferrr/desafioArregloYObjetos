0282c7df474d42d31b5005a37716c5b19257adff
Nicolás <nicog.rivero@gmail.com>
2025-05-17 18:47:36 -0400

    Se añadióel js para las propiedades en alquiler con su bucle for of

---
+++ assets/js/propiedades_alquiler.js
@@ -0,0 +1,39 @@
+import { propiedades_alquiler } from './propiedades.js'
+
+let htmlAlquileres = ''
+
+for (let alquiler of propiedades_alquiler) {
+    htmlAlquileres += `
+        <div class="col-md-4 mb-4">
+            <div class="card">
+                <img src="${alquiler.src}" class="card-img-top" alt="Imagen del departamento"
+                />
+                <div class="card-body">
+                    <h5 class="card-title">${alquiler.nombre}</h5>
+                    <p class="card-text">${alquiler.descripcion}</p>
+                    <p>
+                    <i class="fas fa-map-marker-alt"></i>${alquiler.ubicacion}</p>
+                    <p>
+                    <i class="fas fa-bed"></i> ${alquiler.habitaciones} Habitaciones |
+                    <i class="fas fa-bath"></i> ${alquiler.baños} Baños</p>
+                    <p>
+                    <i class="fas fa-dollar-sign"></i>${alquiler.costo}
+                    </p>
+                    ${alquiler.smoke ? 
+                    `<p class="text-success"><i class="fas fa-smoking"></i> Permitido fumar</p>` 
+                    : `<p class="text-danger"> <i class="fas fa-smoking-ban"></i> No se permite fumar</p>`}
+                    ${alquiler.pets ? 
+                    `<p class="text-success">
+                    <i class="fas fa-paw"></i> Mascotas permitidas
+                    </p>` 
+                    : `<p class="text-danger">
+                    <i class="fas fa-ban"></i> No se permiten mascotas
+                    </p>`}
+                </div>
+            </div>
+        </div>
+    `
+}
+
+const divAlquileres = document.querySelector('.alquileres')
+divAlquileres.innerHTML = htmlAlquileres
