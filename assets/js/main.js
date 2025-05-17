6b46afae513e01c0d4d1749c6a58ea7192e95717
Nicolás <nicog.rivero@gmail.com>
2025-05-16 21:38:19 -0400

    Template creado para las cards de venta

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,39 @@
-import { propiedades_alquiler, propiedades_venta } from './propiedades.js';
+import { propiedades_venta } from './propiedades.js'
+
+let html = ''
+
+for (let venta of propiedades_venta) {
+    html += `
+        <div class="col-md-4 mb-4">
+            <div class="card">
+                <img src="${venta.src}" class="card-img-top" alt="Imagen del departamento"
+                />
+                <div class="card-body">
+                    <h5 class="card-title">${venta.nombre}</h5>
+                    <p class="card-text">${venta.descripcion}</p>
+                    <p>
+                    <i class="fas fa-map-marker-alt"></i>${venta.ubicacion}</p>
+                    <p>
+                    <i class="fas fa-bed"></i> ${venta.habitaciones} Habitaciones |
+                    <i class="fas fa-bath"></i> ${venta.baños} Baños</p>
+                    <p>
+                    <i class="fas fa-dollar-sign"></i>${venta.costo}
+                    </p>
+                ${venta.smoke ? 
+                    `<p class="text-success"><i class="fas fa-smoking"></i> Permitido fumar</p>` 
+                    : `<p class="text-danger"> <i class="fas fa-smoking-ban"></i> No se permite fumar</p>`}
+                ${venta.pets ? 
+                    `<p class="text-success">
+                    <i class="fas fa-paw"></i> Mascotas permitidas
+                    </p>` 
+                    : `<p class="text-danger">
+                    <i class="fas fa-ban"></i> No se permiten mascotas
+                    </p>`}
+                </div>
+            </div>
+        </div>
+    `
+}
+
+const divVentas = document.querySelector('.ventas')
+divVentas.innerHTML = html
